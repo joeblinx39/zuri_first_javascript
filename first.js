768bb5e7a28d2056f854b20c68ca06f72aa16066
joeblinx39 <joeblinx39@gmail.com>
2022-06-22 01:27:08 +0100

    commit to main

---
+++ first.js
@@ -0,0 +1,9 @@
+const fname = "Joseph Bassey John";
+const height = "5.6 ft (66 inches)";
+const nationality = "Nigerian";
+
+document.getElementById("fname").innerHTML = 'My name is ' + fname;
+
+document.getElementById("height").innerHTML = 'My height is ' + height;
+
+document.getElementById("nationality").innerHTML = 'I am  a ' + nationality;
